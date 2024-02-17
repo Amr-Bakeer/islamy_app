97bb83fec9df40a68e6dbd0f8b9cd5020fa65dfc
Amr Bakeer <amrbakeer263@gmail.com>
2024-02-17 05:38:05 +0200

    - Add Radio Screen - Add Tasbeh Screen - Install Localization - Add Settings Screen

--- lib/modules/settings/page/settings_view.dart
+++ lib/modules/settings/page/settings_view.dart
@@ -1,10 +1,59 @@
+import 'package:animated_custom_dropdown/custom_dropdown.dart';
 import 'package:flutter/material.dart';
 
 class SettingsView extends StatelessWidget {
-  const SettingsView({super.key});
+  SettingsView({super.key});
+
+  final List<String> languagesList = [
+    "English",
+    "Arabic",
+  ];
+
+  final List<String> themeMode = [
+    "Dark",
+    "Light",
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Text("home");
+    var theme = Theme.of(context);
+    return Padding(
+      padding: const EdgeInsets.all(
+        30,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(
+            height: 60,
+          ),
+          Text(
+            "Language:",
+            style: theme.textTheme.bodyLarge,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          CustomDropdown<String>(
+            items: languagesList,
+            onChanged: (value) {},
+          ),
+          const SizedBox(
+            height: 60,
+          ),
+          Text(
+            "Theme Mode:",
+            style: theme.textTheme.bodyLarge,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          CustomDropdown<String>(
+            items: themeMode,
+            onChanged: (value) {},
+          ),
+        ],
+      ),
+    );
   }
 }
